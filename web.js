5e2fe15e104f2fad868924e9f0f6056169b38c0a
Sharful Islam <it@sislam.com>
2013-07-13 19:35:39 +0000

    file read test

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
+var fs = require('fs');
 var express = require('express');
-
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World-2!');
+  //response.send('Hello World-2!');
+	fs.readFile('index.html', function (err, data) {
+	  if (err) throw err;
+	  //console.log(data);
+	  response.send(data);
+	});
 });
 
 var port = process.env.PORT || 5000;
